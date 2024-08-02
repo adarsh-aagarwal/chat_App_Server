ab5f0da299a0f9ab24eb886b688bdcde9a04e11f
adarsh-aagarwal <165946667+adarsh-aagarwal@users.noreply.github.com>
2024-08-03 00:38:59 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,12 @@
+
+
+
 const mongoose = require("mongoose");
 
 const connect = () => {
-    return mongoose.connect("mongodb+srv://adarshagarwal27102000:AniwaryaZX%40123@cluster0.oqgh6x8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    return mongoose.connect("add your cluster url here ")
        
 }
 
 module.exports = connect;
-
-
-// const mongoose = require("mongoose");
-
-// const connect = () => {
-//     return mongoose.connect("add your cluster url here ")
-       
-// }
-
-// module.exports = connect;
 
